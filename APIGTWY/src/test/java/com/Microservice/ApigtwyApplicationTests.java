0d57685dbade26f6c45686d587b7e8658f869875
elkin.garces <maurogarcesd@gmail.com>
2019-09-03 13:45:59 -0500

    Hola

---
+++ APIGTWY/src/test/java/com/Microservice/ApigtwyApplicationTests.java
@@ -0,0 +1,38 @@
+package com.Microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import micro.Filters.*;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+	public class ApigtwyApplicationTests {
+
+		public static void main(String[] args) {
+			SpringApplication.run(ApigtwyApplicationTests.class, args);
+		}
+		
+		@Bean
+		public PreFilter preFilter() {
+		    return new PreFilter();
+		}
+		@Bean
+		public PostFilter postFilter() {
+		    return new PostFilter();
+		}
+		@Bean
+		public ErrorFilter errorFilter() {
+		    return new ErrorFilter();
+		}
+		@Bean
+		public RouteFilter routeFilter() {
+		    return new RouteFilter();
+		}
+
+	
+
+}
